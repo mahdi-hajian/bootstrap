3d32068a1c44602fe000d7bf3911375c01e98888
mahdi hajian <mahdi.haj.1375@gmail.com>
2017-09-06 13:57:29 +0430

    ready for creat exmple site

---
+++ bootstrap/Controllers/Exple_site_Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bootstrap.Controllers
+{
+    public class Exple_site_Controller : Controller
+    {
+        // GET: Exple_site_
+        public ActionResult Exple_site()
+        {
+            return View();
+        }
+    }
+}
